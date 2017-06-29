f8b8c107b78c89b2560becccd4099311df3ec268
Gabriel Kozlowaski <gkm.kozlowaski@gmail.com>
2017-06-29 16:21:39 -0300

    Transform Template into a stateless function

--- render/Template.js
+++ render/Template.js
@@ -1,36 +1,37 @@
-import React, { Component, PropTypes } from 'react'
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
 import Sidebar from './Sidebar'
 import Helmet from "react-helmet";
 import './theme.css'
 
-class Template extends Component {
-  render () {
-    return (
-      <div className="font-body">
-        <Helmet
-          script={[
-            {
-              type: 'text/javascript',
-              innerHTML: `
-                window.onload = function () {
-                  var rdom = document.createElement('script')
-                  rdom.src = 'https://unpkg.com/react-dom@15.5.4/dist/react-dom-server.min.js'
-                  document.body.appendChild(rdom)
-                }
-              `
-            }
-          ]}
-          link={[
-              {rel: "stylesheet", href: "http://io.vtex.com.br/fonts/stylesheet.css"},
-          ]}
-        />
-        <Sidebar />
-        <main className="Window fl h-100 w-100 w-80-ns">
-          {this.props.children}
-        </main>
-      </div>
-    )
-  }
+export default function Template({children}) {
+  return (
+    <div className="font-body">
+      <Helmet
+        script={[
+          {
+            type: 'text/javascript',
+            innerHTML: `
+              window.onload = function () {
+                var rdom = document.createElement('script')
+                rdom.src = 'https://unpkg.com/react-dom@15.5.4/dist/react-dom-server.min.js'
+                document.body.appendChild(rdom)
+              }
+            `
+          }
+        ]}
+        link={[
+            {rel: "stylesheet", href: "http://io.vtex.com.br/fonts/stylesheet.css"},
+        ]}
+      />
+      <Sidebar />
+      <main className="ph5 pt4 fl h-100 w-100 w-80-ns">
+        {children}
+      </main>
+    </div>
+  )
 }
 
-export default Template
+Template.propTypes = {
+  children: PropTypes.node.isRequired,
+}
